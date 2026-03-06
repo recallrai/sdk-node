1f2436b1ca3452114ea6e8e6c507eeaa46a3489b
Devasheesh Mishra <devasheeshmishra4@gmail.com>
2026-03-07 03:13:08 +0530

    feat: rename newMemoryContent to proposedMemoryContent in parseMergeConflictResponse for consistency

--- src/merge-conflict.ts
+++ src/merge-conflict.ts
@@ -161,7 +161,7 @@ export class MergeConflict {
 		return {
 			id: conflictData.id,
 			projectUserSessionId: conflictData.project_user_session_id,
-			newMemoryContent: conflictData.new_memory_content,
+			proposedMemoryContent: conflictData.proposed_memory_content,
 			newMemories: conflictData.new_memories?.map((mem: any) => ({
 				memoryId: mem.memory_id,
 				content: mem.content,
